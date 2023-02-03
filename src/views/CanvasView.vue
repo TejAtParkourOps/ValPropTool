b10f233085a1909c205480621d6972613e93b5bc
Tej Birring <tejbirring@gmail.com>
2023-02-03 14:37:11 +0000

    Prettify.

--- src/views/CanvasView.vue
+++ src/views/CanvasView.vue
@@ -47,7 +47,14 @@
           {{ Name }}
         </h2>
         <div class="d-flex flex-row justify-content-end">
-          <b-badge class="mr-2" variant="secondary" href="#" @click="save" v-if="!isOwner && !routePropositionId">Unsaved</b-badge>
+          <b-badge
+            class="mr-2"
+            variant="secondary"
+            href="#"
+            @click="save"
+            v-if="!isOwner && !routePropositionId"
+            >Unsaved</b-badge
+          >
           <b-badge variant="secondary" href="#" @click="publish">{{
             Published ? "Published" : "Unpublished"
           }}</b-badge>
